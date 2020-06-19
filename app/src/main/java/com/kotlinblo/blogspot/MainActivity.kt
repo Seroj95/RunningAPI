898de4ef576eac9e0ec7ed1560b4a034dee0af9f
Seroj <seroj-djan@mail.ru>
2020-06-19 20:53:57 +0400

    Dagger hilt Setup

--- app/src/main/java/com/kotlinblo/blogspot/MainActivity.kt
+++ app/src/main/java/com/kotlinblo/blogspot/MainActivity.kt
@@ -2,10 +2,18 @@ package com.kotlinblo.blogspot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import com.kotlinblo.blogspot.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("run"," RunDao${runDAO.hashCode()}")
     }
 }
