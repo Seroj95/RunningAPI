34ae8bbf22f2cb474c54c6066f18856a5ce0b093
Seroj <seroj-djan@mail.ru>
2020-06-23 19:55:19 +0400

    Setup NAvigation

--- app/src/main/java/com/kotlinblo/blogspot/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/kotlinblo/blogspot/ui/fragments/RunFragment.kt
@@ -1,12 +1,28 @@
 package com.kotlinblo.blogspot.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.kotlinblo.blogspot.R
 import com.kotlinblo.blogspot.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment :Fragment(R.layout.fragment_run){
 private  val viewModel:MainViewModel by viewModels ()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
